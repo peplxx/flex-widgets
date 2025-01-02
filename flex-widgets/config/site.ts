5fa0e05a66e32b47c233893fba2cd083693856a8
peplxx <seregaopkymelnikov@yandex.ru>
2025-01-02 15:25:53 +0300

    chore: fill config

--- flex-widgets/config/site.ts
+++ flex-widgets/config/site.ts
@@ -2,9 +2,8 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Flex-Widgets",
-  description:
-    "Make beautiful personal websites regardless of your design experience.",
+  description: "Flex widgets for custom purposes",
   links: {
-    github: "https://github.com/nextui-org/nextui",
+    github: "https://github.com/peplxx/flex-widgets",
   },
 };
